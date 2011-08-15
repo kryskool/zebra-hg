b09ca3e11fd43412710ec8750e78d8d63e30c8b5
Ben Croston <ben@croston.org>
2011-08-15 18:38:59 +0100

    Works in py 2.6

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ classifiers = ['Development Status :: 3 - Alpha',
                'Operating System :: Unix',
                'License :: OSI Approved :: MIT License',
                'Programming Language :: Python',
+               'Programming Language :: Python :: 2.6',
                'Programming Language :: Python :: 2.7',
                'Programming Language :: Python :: 3',
                'Topic :: Printing']
